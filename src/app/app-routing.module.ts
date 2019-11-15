a4a72135b2155942f0f225db0ab12f560a00a413
Vibhav Gupta <vibhav.gupta@nagarro.com>
2019-11-15 14:02:31 +0530

    commiting demo project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule,PreloadAllModules } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+
+const routes: any = [ 
+    {path:'welcome',component:WelcomeComponent},
+    // {
+    //   path:'products',
+    //   canActivate:[AuthGuard],
+    //   data:{ preload:true },
+    //   loadChildren: ()=>
+    //     import('./products/product.module').then(m=>m.ProductModule)
+    // },
+    // {
+    //   path:'category',
+    //   canActivate:[AuthGuard],
+    //   data:{ preload:true },
+    //   loadChildren: ()=>
+    //     import('./category/category.module').then(m=>m.CategoryModule)
+    // },
+    // {
+    //   path:'merchant',
+    //   canActivate:[AuthGuard],
+    //   data:{ preload:true },
+    //   loadChildren: ()=>
+    //     import('./merchant/merchant.module').then(m=>m.MerchantModule)
+    // },
+    // {
+    //   path:'customer',
+    //   canActivate:[AuthGuard],
+    //   data:{ preload:true },
+    //   loadChildren: ()=>
+    //     import('./customer/customer.module').then(m=>m.CustomerModule)
+    // },
+    // {
+    //   path:'order',
+    //   canActivate:[AuthGuard],
+    //   data:{ preload:true },
+    //   loadChildren: ()=>
+    //     import('./order/order.module').then(m=>m.OrderModule)
+    // },
+    {path:'',redirectTo:'welcome',pathMatch:'full'},
+    {path:'**',redirectTo:'welcome',pathMatch:'full'}
+  ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
