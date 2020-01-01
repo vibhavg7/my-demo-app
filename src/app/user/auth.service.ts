41a3de6f8442b05b4564b2ba45245548a3b6c29d
Vibhav Gupta <vibhav.gupta@nagarro.com>
2020-01-01 23:17:27 +0530

    cc

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -17,12 +17,12 @@ export class AuthService {
     }
     // currentUser: User;
     redirectUrl: any;
-    _authURL: any = "http://ec2-3-134-77-29.us-east-2.compute.amazonaws.com:3000/";
+    _authURL: any = "http://localhost:3000/";
 
     constructor(private _http: HttpClient) {
 
     }
-   
+
 
     login(employee) : Observable<any>
     {
