e161ee70b7ce8dc8a97b7bb5a38cca9ef819de7a
vibhavg7 <vibhav.gupta@nagarro.com>
2020-09-15 04:12:15 +0530

    cc

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -6,57 +6,68 @@ import { throwError, Observable, of } from 'rxjs';
 
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthService {
 
-    get isLoggedIn() {
-        // let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        // return (!!currentUser) ? true : false;
-        return !! localStorage.getItem('currentUser');
-    }
-    // currentUser: User;
-    redirectUrl: any;
-    _authURL: any = 'https://api.grostep.com/';
+  get isLoggedIn() {
+    // let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    // return (!!currentUser) ? true : false;
+    return !!localStorage.getItem('currentUser');
+  }
+  // currentUser: User;
+  redirectUrl: any;
+  authURL: any = 'https://api.grostep.com/employeeapi';
 
-    constructor(private _http: HttpClient) {
+  constructor(private http: HttpClient) {
 
-    }
+  }
 
 
-    login(employee) : Observable<any>
-    {
-        return this._http.post(`${this._authURL}employeeapi/validate`, employee).pipe(
-            tap(),
-            map((employeeData) => {
-                if (employeeData['status'] === 200 && employeeData['employeeData'][0] != undefined) {
-                    console.log('not a valid user' + employeeData['employeeData'][0]);
-                    localStorage.setItem('currentUser', JSON.stringify(employeeData['employeeData'][0]));
-                }
-                return employeeData;
-            })
-        )
-
-    }
-
-    logout() {
-        // remove user from local storage to log user out
-        localStorage.removeItem('currentUser');
-    }
-
-    private handleError(err: HttpErrorResponse) {
-        // in a real world app, we may send the server to some remote logging infrastructure
-        // instead of just logging it to the console
-        let errorMessage = '';
-        if (err.error instanceof ErrorEvent) {
-            // A client-side or network error occurred. Handle it accordingly.
-            errorMessage = `An error occurred: ${err.error.message}`;
-        } else {
-            // The backend returned an unsuccessful response code.
-            // The response body may contain clues as to what went wrong,
-            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+  login(employee): Observable<any> {
+    return this.http.post(`${this.authURL}/validate`, employee).pipe(
+      tap(),
+      map((employeeData: any) => {
+        if (employeeData.status === 200 && employeeData.employeeData[0] !== undefined) {
+          localStorage.setItem('currentUser', JSON.stringify(employeeData.employeeData[0]));
         }
-        console.error(errorMessage);
-        return throwError(errorMessage);
+        return employeeData;
+      })
+    );
+  }
+
+  getAllEmployeeDashboard(employeeId: any) {
+    return this.http.get<any[]>(`${this.authURL}/dashboards/${employeeId}`)
+      .pipe(
+        tap(data => {
+          // console.log(JSON.stringify(data));
+        })
+        , map((data: any) => {
+          return data;
+
+        })
+        , catchError(this.handleError)
+      );
+  }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
     }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 }
--- src/app/user/dashboard.component.ts
+++ src/app/user/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +9,19 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private route: Router) { }
+  dashboardDataCount: any;
+  dashboardData: any;
+  constructor(private route: Router, private auth: AuthService) { }
 
   ngOnInit() {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const employeeId = currentUser.employee_id;
+    this.auth.getAllEmployeeDashboard(employeeId).subscribe((data: any) => {
+      if (data.status === 200) {
+        this.dashboardData = data.employee_dashboard_info;
+        this.dashboardDataCount = data.employee_dashboard_count[0].employee_dashboard_count;
+      }
+    });
   }
 
   dashboardClick(category) {
