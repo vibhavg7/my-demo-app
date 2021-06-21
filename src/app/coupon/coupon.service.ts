e63e518f6aedafa4c165d13afb0dd1a8ec0adc65
vibhavg7 <vibhav.gupta@nagarro.com>
2021-06-21 12:38:44 +0530

    cc

--- src/app/coupon/coupon.service.ts
+++ src/app/coupon/coupon.service.ts
@@ -10,7 +10,7 @@ import { ErrorTracker } from '../shared/errorTracker';
 export class CouponService {
 
   constructor(private http: HttpClient) { }
-  private couponServiceUrl = 'http://13.233.10.240:3000/vouchersapi/';
+  private couponServiceUrl = 'https://api.grostep.com/v1/vouchersapi/';
 
   fetchAllCoupons(pageNumber: number, pageSize: any, filterBy: any): Observable<any> {
     const obj: any = {};
